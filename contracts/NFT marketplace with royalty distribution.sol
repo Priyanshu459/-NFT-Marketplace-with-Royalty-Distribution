546e7dad29e66faf87587b4e8f4a4320bef5d740
Priyanshu Patel <priyanshuabhi56@gmail.com>
2025-08-12 11:10:58 +0530

    Update NFT marketplace with royalty distribution.sol

--- contracts/NFT marketplace with royalty distribution.sol
+++ contracts/NFT marketplace with royalty distribution.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import  "@openzeppelin/contracts/access/Ownable.sol";
+import   "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NFTMarketplaceWithRoyalty is ERC721URIStorage, Ownable {
     uint256 public tokenCounter;
